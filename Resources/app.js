f8e8d47bc8ea122c47cdc808e17723798af48824
Hemstreet <jonhemstreet@gmail.com>
2012-05-28 14:03:58 -0600

    updating

--- Resources/app.js
+++ Resources/app.js
@@ -1,126 +1,81 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-	Titanium.UI.setBackgroundColor('#000');
-	Titanium.UI.iPhone.hideStatusBar(); 
-	
-// create tab group
-	var tabGroup = Titanium.UI.createTabGroup();
-
-//
-// Windows
-//
+/**
+ * A tabbed application, consisting of multiple stacks of windows associated with tabs in a tab group.  
+ * A starting point for tab-based application with multiple top-level windows. 
+ * Requires Titanium Mobile SDK 1.8.0+.
+ * 
+ * In app.js, we generally take care of a few things:
+ * - Bootstrap the application with any data we need
+ * - Check for dependencies like device type, platform version or network connection
+ * - Require and open our top-level UI component
+ *  
+ */
  
-// Create About
-	// Create Window for About
-	var about = Titanium.UI.createWindow
-	({  
-	    title:'About',
-	    navBarHidden: true 
-	});	
-
-	// Create Arrae Logo
-	var arraeLogo = Titanium.UI.createImageView
-	({
-		image: "./images/arrae.png",
-		left: 0,
-		top: 50,
-		width: 300,
-	});
-
-	// Add in Arrae Logo variable
-	about.add(arraeLogo);
-
-	// Create the nav Tab
-	var aboutTab = Titanium.UI.createTab
-	({  
-    	icon:'./images/tab_about.png',
-    	title:'About',
-   		window:about
-	});
-
-	// Name nav Tab
-	var aboutLabel = Titanium.UI.createLabel
-	({
-		color:'#999',
-		text:'Vibrational App Built By Arrae',
-		font:
-		{
-			fontSize:20,
-			fontFamily:'Helvetica Neue'
-		},
-		textAlign:'center'
-	});
-
-	// Add in label for tab
-	about.add(aboutLabel);
+// determine platform and form factor and render approproate components
+var osname = Ti.Platform.osname,
+	version = Ti.Platform.version,
+	height = Ti.Platform.displayCaps.platformHeight,
+	width = Ti.Platform.displayCaps.platformWidth;
 
 
-// Create Profile
-	// Create Window for Profile
-	var profile = Titanium.UI.createWindow
-	({  
-  		title:'Profile',
-    	navBarHidden: true
-	});
-	
-	// Create the nav Tab
-	var profileTab = Titanium.UI.createTab
-	({  
-   		icon:'./images/tab_profile.png',
-   		title:'Profile',
-    	window:profile
-	});
+/**
+ * This sets the background color of the master UIView (when there are no windows/tab groups on it)
+ * After the user identifies their vibrational color, we'll want the background color set to reflect that
+ */ 
+Titanium.UI.setBackgroundColor('#000');
 
-	// Name nav tab
-	var profileLabel = Titanium.UI.createLabel({
-		color:'#999',
-		text:'Profile Content',
-		font:
-		{
-			fontSize:20,
-			fontFamily:'Helvetica Neue'
-		},
-		textAlign:'center'
-	});
-	
-	// Add in label for tab
-	profile.add(profileLabel);
 
-// Create Compass
-	// Create Window for Compass
-	var compass = Titanium.UI.createWindow
-	({  
-    	title:'Compass',
-    	url: 'geolocation.js',
-    	navBarHidden: true
-	});
-	
-	// Create the nav Tab
-	var compassTab = Titanium.UI.createTab({  
-    	icon:'./images/tab_compass.png',
-    	title:'Compass',
-    	window:compass
-	});
+/**
+ * Create Application Windows
+ * 
+ * Windows are loaded as static .js files like a static HTML site would be
+ * All we have to do here is create the wrappers / window object to correspond with a tab
+ */
+var about = Titanium.UI.createWindow({  
+    title: L('tab-about'),
+    url: 'about.js',
+    navBarHidden: true
+});	
 
-	// Name nav Tab
-	var compassLabel = Titanium.UI.createLabel({
-		color:'#999',
-		font:
-		{
-			fontSize:20,
-			fontFamily:'Helvetica Neue'
-		}
-	});	
+var profile = Titanium.UI.createWindow({  
+	title: L('tab-profile'),
+	url: 'profile.js',
+	navBarHidden: true
+});
 
-	// Add in label for compass tab
-	compass.add(compassLabel);
+var compass = Titanium.UI.createWindow({  
+	title: L('tab-location'),
+	url: 'geolocation.js',
+	navBarHidden: true
+});
 
-//
-// Tabs
-//
-	tabGroup.addTab(aboutTab);  
-	tabGroup.addTab(profileTab);  
-	tabGroup.addTab(compassTab);  
 
-// open tab group
-	tabGroup.open();
-	
+/**
+ * Creat Application Tabs
+ * 
+ * Tabs are auto rendered and formatted on each device matching the most native settings / feel
+ */
+var tabGroup = Titanium.UI.createTabGroup();
+
+var aboutTab = Titanium.UI.createTab({  
+	icon: './images/tab_about.png',
+	title: 'About',
+	window: about
+});
+
+var profileTab = Titanium.UI.createTab({  
+	icon: './images/tab_profile.png',
+	title: 'Profile',
+	window: profile
+});
+
+var compassTab = Titanium.UI.createTab({  
+	icon:'./images/tab_compass.png',
+	title:'Compass',
+	window:compass
+});
+
+tabGroup.addTab(aboutTab);  
+tabGroup.addTab(profileTab);  
+tabGroup.addTab(compassTab);  
+
+tabGroup.open();	
