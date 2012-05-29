6d34199ed6cdcc7e8cae19c0dccb2e7432436237
Clay McIlrath <clay.mcilrath@gmail.com>
2012-05-29 10:36:17 -0600

    Updated the profile page to pull a web url, and updated language and image files

--- Resources/profile.js
+++ Resources/profile.js
@@ -1,29 +1,12 @@
 var win = Ti.UI.currentWindow;
-
 win.hideNavBar();
 
-// Create Arrae Logo
-var arraeLogo = Titanium.UI.createImageView({
-	image: "./images/arrae.png",
-	left: 0,
-	top: 25,
-	width: 300,
-	height : 'auto'
-});
-// Add in Arrae Logo variable
-win.add(arraeLogo);
+// for now I'm going to set this to false, but later, we'll want to check the database
+var profile = false;
 
-// Name nav Tab
-var label = Titanium.UI.createLabel({
-	color: '#999',
-	text: 'Profile Content',
-	font:
-	{
-		fontSize:20,
-		fontFamily:'Helvetica Neue'
-	},
-	textAlign: 'center'
-});
 
-// Add in label for tab
-win.add(label);
+var webview = Titanium.UI.createWebView({ url: 'http://staging.arr.ae/vibrational' });
+var window = Titanium.UI.createWindow();
+
+window.add(webview);
+window.open();
